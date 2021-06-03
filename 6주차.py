0f2a7fce6b59f3149297d8ff712bdeb266855c56
waroad <doha2012@naver.com>
2021-06-04 08:02:22 +0900

    d

--- 6주차.py
+++ 6주차.py
@@ -4,15 +4,11 @@ import requests
 headers = {
     'User-Agent': 'Not Crawling X'
 }
-
 response = requests.get('https://kin.naver.com/', headers=headers).text
 soup = bs4.BeautifulSoup(response, 'html.parser')
 trends = soup.select('#rankingChart > ul > li')
 
-rank = lambda trend: int(trend.select_one('span.no').text)
-content = lambda trend: trend.select_one('a.ranking_title').text
-
 with open('kin_trend.csv', 'w') as file:
     file.write("순위, 이름\n")
-    for trend in sorted(trends, key=rank):
-        file.write(f'{rank(trend)}위, {content(trend)}\n')
+    for trend in trends:
+        file.write(f'{int(trend.select_one("span.no").text)}위, {trend.select_one("a.ranking_title").text}\n')
